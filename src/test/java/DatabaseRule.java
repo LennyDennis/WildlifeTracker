311de9745b48f416b74082d3432a8099b6869606
LennyDennis <lennydennis@gmail.com>
2020-02-13 23:18:26 +0300

    Create a DBRule class that connects to the test db

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -6,6 +6,7 @@ public class DatabaseRule extends ExternalResource {
     protected void before(){
         DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "lenny", " ");
     }
+
     @Override
     protected void after(){
         try(Connection con = DB.sql2o.open()) {
