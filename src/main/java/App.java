b5dc3fd538866d639b3890c3e69603f85e5d5266
LennyDennis <lennydennis@gmail.com>
2020-02-14 07:19:14 +0300

    Create App.java that has routes linking to the homepage, tables and forms of the animals

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,7 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
         Map<String,Object> model = new HashMap<String,Object>();
 
         get("/", (req,res) -> {
