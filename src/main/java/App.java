27472ec0552b02860a294bfe27c25ed8e67b8bbc
LennyDennis <lennydennis@gmail.com>
2020-02-14 07:20:04 +0300

    Add spark routes to the sighting form and tables in the App java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,7 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+
         staticFileLocation("/public");
 
         Map<String,Object> model = new HashMap<String,Object>();
