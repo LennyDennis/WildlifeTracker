88a0cc2ed2daeb6fd4a9b69b2fbc38e392559578
LennyDennis <lennydennis@gmail.com>
2020-02-17 14:55:22 +0300

    Change the host in the heroku db path

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -3,7 +3,7 @@ import org.sql2o.*;
 public class DB {
 
 //    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "lenny", " ");
-    public static String connectionString = "jdbc:postgresql://igcdveckmrjegr:15e056595699a9b71cbe5cd1a52804383d329c01c73e3311dbe1f4471a9620b9@ec2-50-17-178-87.compute-1.amazonaws.com:5432/d4j2d175a39ddv";
+    public static String connectionString = "jdbc:postgresql://ec2-50-17-178-87.compute-1.amazonaws.com:5432/d4j2d175a39ddv";
     public static Sql2o sql2o = new Sql2o(connectionString, "igcdveckmrjegr", "15e056595699a9b71cbe5cd1a52804383d329c01c73e3311dbe1f4471a9620b9");
 
 }
